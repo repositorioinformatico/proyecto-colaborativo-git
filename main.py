cc78f8b1c97e525519ebc65fd27e810864c6ffd4
repositorioinformatico <e@e.com>
2023-11-08 16:48:26 +0000

    Mis cambios en main

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
 import base64
 
-def base16_JorgeG(input_string):
+def base16_Jorge(input_string):
     hexadecimal = ''.join([f'{ord(char):02x}' for char in input_string])
     return hexadecimal
 
-def base64thomas(y):
+def base64_Thomas(y):
     return base64.b64encode(y.encode()).decode()
 
-def base64-fede():
-   texto base64.b64encode(bytes("hola Buenas tardes soy Federico"))
+def base64_Fede():
+    base64.b64encode(bytes("hola Buenas tardes soy Federico"))
     base64_str = texto.decode('utf-8')
     print(base64_str)
     
 def main():
     entrada = input("Introduce texto a convertir a base64: ")
-    print(base64thomas(entrada))
+    print(base64_Thomas(entrada))
+    #print(base64_Fede())
+    print(base16_Jorge(entrada))
 
 if __name__ == "__main__":
     main()
