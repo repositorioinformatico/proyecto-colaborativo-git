5e5096b2946542106ab95bf3c3e2293596ab9c4a
FedericoJB <federicojosebaldoni@gmail.com>
2023-11-08 17:14:12 +0100

    fet: correccion del print

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ import base64
 def base64-fede():
    texto base64.b64encode(bytes("hola Buenas tardes soy Federico"))
     base64_str = texto.decode('utf-8')
-    print base64_str
+    print(base64_str)
+    
+base64-fede()
