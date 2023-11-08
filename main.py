b2575d14d0ec4c8d8cf6cb9b3dc7a5b11a41ea7a
ceti <ceti@ceti.com>
2023-11-08 16:09:11 +0000

    Base16 JorgeGadeaGomez

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 print("Funciona")
+def base16_JorgeG(input_string):
+    hexadecimal = ''.join([f'{ord(char):02x}' for char in input_string])
+    return hexadecimal
+
+input_string = "Jorge Gadea Gómez"
+print("String original:", input_string)
+print("Representación hexadecimal:", base16_JorgeG(input_string))
+
