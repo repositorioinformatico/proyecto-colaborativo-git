ec0514542bef2a6b653a5a281b5bf6b661ee8e0f
Thomas <thoboo@alu.edu.gva.es>
2023-11-08 17:26:02 +0100

    Anadido funcion Thomas

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 print("Funciona")
+
+import base64
+
+def base64thomas(y):
+    return base64.b64encode(y.encode()).decode()
+
+def main():
+    entrada = input("Introduce texto a convertir a base64: ")
+    print(base64thomas(entrada))
+
+if __name__ == "__main__":
+    main()
