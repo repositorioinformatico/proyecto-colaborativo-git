f521e2c06af8253470d3e7f11e47c27b410cd64c
miguelalepuz <maq.peremaria@gmail.com>
2023-11-08 17:23:15 +0100

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 print("Funciona")
+
+import base64
+
+texto = "buenos dias"
+texto_codificado = base64.b64encode(texto.encode('utf-8'))
+
+print ("texto original:", = texto)
+print ("texto codificado en base-64:", texto_codificado)
+
